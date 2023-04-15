3e1c3182222519915e4b17395dad134045a003a6
TokisakiKurumi2001 <minhkhoile2001nsg@gmail.com>
2023-04-15 05:21:41 +0000

    Training code

--- train.py
+++ train.py
@@ -1,26 +1,31 @@
 from pytorch_lightning.loggers import WandbLogger
 import pytorch_lightning as pl
-from ReWord import ReWordDataLoader, LitReWord
+from ParaBLEU import ParaBLEUPDataLoader, LitParaBLEU
 
 if __name__ == "__main__":
-    wandb_logger = WandbLogger(project="proj_reorder_word")
+    wandb_logger = WandbLogger(project="proj_new_dummy")
 
     hyperparameter = {
-        'pretrained_ck': 'roberta-base',
-        'method_for_layers': 'mean',
-        'layers_use_from_last': 2,
-        'lr': 3e-5
+        "encoder_ckpt": "xlm-roberta-base",
+        "gen_ckpt": "facebook/m2m100_418M",
+        "lr": 3e-4,
+        "alpha": 4.0,
+        "beta": 10.0
     }
-    lit_reword = LitReWord(**hyperparameter)
+    lit_parableu = LitParaBLEU(**hyperparameter)
 
     # dataloader
-    reword_dataloader = ReWordDataLoader(pretrained_ck='roberta-base', max_length=25)
-    [train_dataloader, test_dataloader] = reword_dataloader.get_dataloader(batch_size=128, types=["train", "test"])
+    parableu_pretrained_dataloader = ParaBLEUPDataLoader(
+        encoder_ckpt=hyperparameter['encoder_ckpt'],
+        gen_ckpt=hyperparameter['gen_ckpt'],
+        max_length=128
+    )
+    [train_dataloader, test_dataloader, valid_dataloader] = parableu_pretrained_dataloader.get_dataloader(batch_size=64, types=["train", "test", "validation"])
 
     # train model
-    trainer = pl.Trainer(max_epochs=90, devices=[0], accelerator="gpu", logger=wandb_logger, log_every_n_steps=20)
-    trainer.fit(model=lit_reword, train_dataloaders=train_dataloader, val_dataloaders=test_dataloader)
+    trainer = pl.Trainer(max_epochs=2, devices=[0], accelerator="gpu", logger=wandb_logger, val_check_interval=3000)
+    trainer.fit(model=lit_parableu, train_dataloaders=train_dataloader, val_dataloaders=valid_dataloader)
     trainer.test(dataloaders=test_dataloader)
 
     # save model & tokenizer
-    lit_reword.export_model('reword_model/v1')
+    lit_parableu.export_model('parableu_model/v1')
